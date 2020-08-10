7d32da23034702ad7995b928cf69e36588822f48
Coder Sathy <69349524+CoderSathy@users.noreply.github.com>
2020-08-10 18:00:55 +0545

    Add files via upload

---
+++ snackbar.js
@@ -0,0 +1,7 @@
+function snackFunc(str) {
+  document.body.innerHTML += '<div id="snackbar">'+str+'</div>';
+  var x = document.getElementById("snackbar");
+  x.className = "show";
+
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
