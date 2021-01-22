0a74068c082dbfbe046abd016a84ad548140989d
Coder Sathy <69349524+CoderSathy@users.noreply.github.com>
2021-01-22 12:43:03 +0545

    Added time Support

--- snackbar.js
+++ snackbar.js
@@ -1,7 +1,7 @@
-function Snackbar(str) {
+function Snackbar(str,time) {
   document.body.innerHTML += '<div id="snackbar">'+str+'</div>';
   var x = document.getElementById("snackbar");
   x.className = "show";
 
-  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, time);
 }
